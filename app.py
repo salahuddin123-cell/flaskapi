ff8ddfc2991af053edcc92bc4e286e897fd89f54
salahuddin <salahuddin934@gmail.com>
2023-04-07 11:23:02 +0530

    sds

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template
 from io import BytesIO
 from flask_cors import CORS
 
@@ -7,7 +7,7 @@ from pdf2docx import parse
 from flask import Flask, request, json
 from pytube import YouTube
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 CORS(app)
 # cors = CORS(app, resource={
 #     r"/*":{
@@ -16,48 +16,51 @@ CORS(app)
 # })
 app.config['UPLOAD_FOLDER'] = 'static/img/'
 
-@app.route('/<int:number>/')
-def incrementer(number):
-    return "Incremented number is " + str(number+1)
 
 @app.route('/')
-def front_page():
-    return "hello dosto"
+def showtemplate():
+    return render_template('index.html')
+
+
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      filename = "my.pdf"
-      f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = "my.pdf"
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     #   f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+        return 'file uploaded successfully'
 
-@app.route('/convert',methods=["GET","POST"])
+
+@app.route('/convert', methods=["GET", "POST"])
 def convert_file():
 
-        pdf_file = "static/img/my.pdf"
-        docx_file="static/docx/random.docx"
-        # docx_file = os.path.expanduser(f"~/Downloads/random.docx")
+    pdf_file = "static/img/my.pdf"
+    docx_file = "static/docx/random.docx"
+    # docx_file = os.path.expanduser(f"~/Downloads/random.docx")
 
-        # convert pdf to docx
-        parse(pdf_file, docx_file)
+    # convert pdf to docx
+    parse(pdf_file, docx_file)
 
-        return send_file(docx_file,as_attachment=True)
+    return send_file(docx_file, as_attachment=True)
 
-@app.route('/download',methods=["GET","POST"])
 
+@app.route('/download', methods=["GET", "POST"])
 def download():
     if request.method == 'POST':
 
-        url=json.loads(request.data)
-    
-        yt = YouTube(url['data'])
+        # url = json.loads(request.data)
+        url=request.form["url"]
+
+        yt = YouTube(url)
         file_name = 'my_video.mp4'
-        video=yt.streams.get_lowest_resolution().download(filename=file_name)
-        fname=video.split("//")[-1]
+        video = yt.streams.get_lowest_resolution().download(filename=file_name)
+        fname = video.split("//")[-1]
+        print(fname)
         return send_file(
             fname,
             as_attachment=True)
+
 
 
 if __name__ == '__main__':
